913ae21c754a8d9ac04708b6c25d91e67f2beadf
jonilsondeveloper <jonilsondeveloper@gmail.com>
2019-03-05 21:33:40 -0300

    banco de dados configurado

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: '127.0.0.1',
   username: 'docker',
   password: 'docker',
-  database: 'gonodemodulo2',
+  database: 'gonodemodulo2desafio',
   operatorsAliases: false,
   define: {
     timestamps: true,
